546be835874328b0526927669fd6256a4b2d7b8e
JuanPablooo <juanpablopereiragomes@hotmail.com>
2020-11-01 23:10:15 -0300

    refactore(home): list vehicles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import NavBar from '../../components/NavBar'
+import NavBar from '../../components/NavBar';
+import VehicleView from '../../components/VehicleView';
+
+import { vehicle }  from '../../services/index';
 
 function App() {
+  const [cars, setCars] = useState([]);
+  const findCars = async ()=>{
+      const { data } = await vehicle.list();
+      console.log(data);
+      setCars(data.content)
+  }
+  useEffect(()=>{
+    findCars()
+
+
+  }, []);
   return (
     <div className="">
       <NavBar/>
       <header className="App-header">
-        <button className="btn btn-primary">ssd</button>
+        {
+          cars.map((car)=> <VehicleView car={car}/>)
+        }
       </header>
     </div>
   );
