aacc1e8dba69dbcb13e5294dc6a83ac5582d8e11
JuanPablooo <juanpablopereiragomes@hotmail.com>
2020-11-02 00:34:41 -0300

    refactore(vehicle) add image default

--- src/components/VehicleView/index.js
+++ src/components/VehicleView/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
+import imageDefault from '../../images/no-image.jpg';
+import './index.css';
 
 const Vehicleiew = ( {vehicle} ) => {
 
     const { thumbnail, name, description, price } = vehicle;
+    
     return(
-        
     <div className="card" >
-        {
-            console.log(vehicle)
-        }
-        <img src={ thumbnail } class="card-img-top" alt="..."/>
-        <div class="card-body">
-            <h5 class="card-title">{name}</h5>
-            <h3 class="card-text">{description}</h3>
-            <h5 class="card-text">{price}</h5>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <img src={ thumbnail ? thumbnail: imageDefault  } className="card-img-top" alt={name}/>
+        <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <h3 className="card-text">{description}</h3>
+            <h5 className="card-text">{price}</h5>
+            <a href="#" className="btn btn-primary"> + Detalhes</a>
         </div>
     </div>
-    )
+    );
 }
 
 export default Vehicleiew;
